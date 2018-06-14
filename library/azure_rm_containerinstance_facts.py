1e635fdf6758d7211f8d4b8e2c56ceca2f18b937
zikalino <zikalino@microsoft.com>
2018-06-14 11:57:04 +0800

    fix stupid thing

--- library/azure_rm_containerinstance_facts.py
+++ library/azure_rm_containerinstance_facts.py
@@ -218,7 +218,7 @@ class AzureRMContainerGroupsFacts(AzureRMModuleBase):
                 # state
             }
         else:
-            return self.as_dict(item)
+            return item.as_dict(item)
 
 
 def main():
